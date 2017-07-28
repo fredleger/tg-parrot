103ba3c66f85a970a4ed1207ec423e854fdb8f7a
Frederic Leger <frederic@webofmars.com>
2017-07-28 09:56:27 +0200

    Fixed Repeat Frequency Bug

--- src/github.com/fredleger/CocoTelegramParrotBot/cocobot/CocoTheParrot.go
+++ src/github.com/fredleger/CocoTelegramParrotBot/cocobot/CocoTheParrot.go
@@ -4,24 +4,33 @@ import (
     "os"
     "log"
     "gopkg.in/telegram-bot-api.v4"
+    "github.com/davecgh/go-spew/spew"
     "github.com/fredleger/CocoTelegramParrotBot/parrotlib"
-    // "github.com/davecgh/go-spew/spew"
 )
 
-func main() {
+var TgToken string = ""
+var debug bool = false
 
-    var TgToken string
+func getConfig() {
+    // Telegram token
     TgToken = os.Getenv("TG_TOKEN")
     if (len(TgToken) <= 0) {
         log.Panic("Invalid (null) telegram Token !")
     }
+}
+
+func main() {
+
+    getConfig()
+
+    spew.Config.Indent = "\t"
 
     bot, err := tgbotapi.NewBotAPI(TgToken)
     if err != nil {
         log.Panic(err)
     }
 
-    bot.Debug = false
+    bot.Debug = debug
 
     log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -34,6 +43,7 @@ func main() {
 
     updates, err := bot.GetUpdatesChan(u)
 
+    // looping around all events
     for update := range updates {
         if update.Message == nil {
             continue
@@ -43,7 +53,7 @@ func main() {
         coco.AddUser(update.Message.From.UserName)
 
         if coco.WillRepeat() {
-            log.Printf("Parrot is awake !!!")
+            log.Printf("Parrot will repeat this time ...")
             msg := tgbotapi.NewMessage(update.Message.Chat.ID, coco.Repeat(update.Message.Text))
             bot.Send(msg)
         } else {
